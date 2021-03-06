0aa260d136306729a3a9a8b5103bd50afd395336
Alvaro Araujo <alvfork@gmail.com>
2021-03-06 02:02:31 -0400

    chore(docs): build landing-page

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,121 +1,57 @@
-// /**
-//  * Copyright (c) Facebook, Inc. and its affiliates.
-//  *
-//  * This source code is licensed under the MIT license found in the
-//  * LICENSE file in the root directory of this source tree.
-//  */
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-// const React = require('react');
+const React = require('react');
 
-// class Footer extends React.Component {
-//   docUrl(doc) {
-//     const {baseUrl} = this.props.config;
-//     const {docsUrl} = this.props.config;
-//     const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-//     return `${baseUrl}${docsPart}${doc}`;
-//   }
+class Footer extends React.Component {
+  docUrl(doc) {
+    const {baseUrl} = this.props.config;
+    const {docsUrl} = this.props.config;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    return `${baseUrl}${docsPart}${doc}`;
+  }
 
-//   render() {
-//     return (
-//       <footer className="nav-footer" id="footer">
-//         <section className="sitemap">
-//           <a href={this.props.config.baseUrl} className="nav-home">
-//             {this.props.config.footerIcon && (
-//               <img
-//                 src={this.props.config.baseUrl + this.props.config.footerIcon}
-//                 alt={this.props.config.title}
-//                 width="66"
-//                 height="58"
-//               />
-//             )}
-//           </a>
-//           <div>
-//             <h5>Docs</h5>
-//             <a href={this.docUrl('doc1.html')}>
-//               Getting Started (or other categories)
-//             </a>
-//             <a href={this.docUrl('doc2.html')}>Guides (or other categories)</a>
-//             <a href={this.docUrl('doc3.html')}>
-//               API Reference (or other categories)
-//             </a>
-//           </div>
-//           <div>
-//             <h5>Community</h5>
-//             <a href={`${this.props.config.baseUrl}users`}>User Showcase</a>
-//             <a
-//               href="https://stackoverflow.com/questions/tagged/"
-//               target="_blank"
-//               rel="noreferrer noopener"
-//             >
-//               Stack Overflow
-//             </a>
-//             <a href="https://discordapp.com/">Project Chat</a>
-//             <a
-//               href="https://twitter.com/"
-//               target="_blank"
-//               rel="noreferrer noopener"
-//             >
-//               Twitter
-//             </a>
-//           </div>
-//           <div>
-//             <h5>More</h5>
-//             <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-//             <a href="https://github.com/">GitHub</a>
-//             <a
-//               className="github-button"
-//               href={this.props.config.repoUrl}
-//               data-icon="octicon-star"
-//               data-count-href="/facebook/docusaurus/stargazers"
-//               data-show-count="true"
-//               data-count-aria-label="# stargazers on GitHub"
-//               aria-label="Star this project on GitHub"
-//             >
-//               Star
-//             </a>
-//             {this.props.config.twitterUsername && (
-//               <div className="social">
-//                 <a
-//                   href={`https://twitter.com/${this.props.config.twitterUsername}`}
-//                   className="twitter-follow-button"
-//                 >
-//                   Follow @{this.props.config.twitterUsername}
-//                 </a>
-//               </div>
-//             )}
-//             {this.props.config.facebookAppId && (
-//               <div className="social">
-//                 <div
-//                   className="fb-like"
-//                   data-href={this.props.config.url}
-//                   data-colorscheme="dark"
-//                   data-layout="standard"
-//                   data-share="true"
-//                   data-width="225"
-//                   data-show-faces="false"
-//                 />
-//               </div>
-//             )}
-//           </div>
-//         </section>
+  render() {
+    const {config} = this.props;
 
-//         <a
-//           href="https://opensource.facebook.com/"
-//           target="_blank"
-//           rel="noreferrer noopener"
-//           className="fbOpenSource"
-//         >
-//           <img
-//             src={`${this.props.config.baseUrl}img/oss_logo.png`}
-//             alt="Facebook Open Source"
-//             width="170"
-//             height="45"
-//           />
-//         </a>
-//         <section className="copyright">{this.props.config.copyright}</section>
-//       </footer>
-//     );
-//   }
-// }
+    return (
+      <footer className="nav-footer" id="footer">
+        <section className="sitemap">
+          <a href="https://www.npmjs.com/package/delfos" className="nav-home">
+            {config.footerIcon && (
+              <img
+                src={config.baseUrl + config.footerIcon}
+                alt={config.title}
+                width="66"
+                height="58"
+              />
+            )}
+          </a>
+          <div>
+            <a href="https://github.com/">GitHub</a>
+            <a
+              className="github-button"
+              href={config.repoUrl}
+              data-icon="octicon-star"
+              data-count-href="/facebook/docusaurus/stargazers"
+              data-show-count="true"
+              data-count-aria-label="# stargazers on GitHub"
+              aria-label="Star this project on GitHub"
+            >
+              Star
+            </a>
+          </div>
+        </section>
+        {/* <section className="copyright">{config.copyright}</section> */}
+      </footer>
+    );
+  }
+}
 
-// module.exports = Footer;
+
+
+module.exports = Footer;
--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -1,213 +1,199 @@
-// /**
-//  * Copyright (c) Facebook, Inc. and its affiliates.
-//  *
-//  * This source code is licensed under the MIT license found in the
-//  * LICENSE file in the root directory of this source tree.
-//  */
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-// const React = require('react');
+const React = require('react');
 
-// const CompLibrary = require('../../core/CompLibrary.js');
+const CompLibrary = require('../../core/CompLibrary.js');
 
-// const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
-// const Container = CompLibrary.Container;
-// const GridBlock = CompLibrary.GridBlock;
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
 
-// class HomeSplash extends React.Component {
-//   render() {
-//     const {siteConfig, language = ''} = this.props;
-//     const {baseUrl, docsUrl} = siteConfig;
-//     const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-//     const langPart = `${language ? `${language}/` : ''}`;
-//     const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
 
-//     const SplashContainer = (props) => (
-//       <div className="homeContainer">
-//         <div className="homeSplashFade">
-//           <div className="wrapper homeWrapper">{props.children}</div>
-//         </div>
-//       </div>
-//     );
+    const SplashContainer = (props) => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
 
-//     const Logo = (props) => (
-//       <div className="projectLogo">
-//         <img src={props.img_src} alt="Project Logo" />
-//       </div>
-//     );
+    const Logo = (props) => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
 
-//     const ProjectTitle = (props) => (
-//       <h2 className="projectTitle">
-//         {props.title}
-//         <small>{props.tagline}</small>
-//       </h2>
-//     );
+    const ProjectTitle = (props) => (
+      <h2 className="projectTitle">
+        {props.title}
+        <small>{props.tagline}</small>
+      </h2>
+    );
 
-//     const PromoSection = (props) => (
-//       <div className="section promoSection">
-//         <div className="promoRow">
-//           <div className="pluginRowBlock">{props.children}</div>
-//         </div>
-//       </div>
-//     );
+    const PromoSection = (props) => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
 
-//     const Button = (props) => (
-//       <div className="pluginWrapper buttonWrapper">
-//         <a className="button" href={props.href} target={props.target}>
-//           {props.children}
-//         </a>
-//       </div>
-//     );
+    const Button = (props) => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
 
-//     return (
-//       <SplashContainer>
-//         <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
-//         <div className="inner">
-//           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
-//           <PromoSection>
-//             <Button href="#try">Try It Out</Button>
-//             <Button href={docUrl('doc1.html')}>Example Link</Button>
-//             <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-//           </PromoSection>
-//         </div>
-//       </SplashContainer>
-//     );
-//   }
-// }
+    return (
+      <SplashContainer>
+        <Logo img_src={`${baseUrl}img/undraw_monitor.png`} />
+        <div className="inner">
+          <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
+        </div>
+      </SplashContainer>
+    );
+  }
+}
 
-// class Index extends React.Component {
-//   render() {
-//     const {config: siteConfig, language = ''} = this.props;
-//     const {baseUrl} = siteConfig;
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
 
-//     const Block = (props) => (
-//       <Container
-//         padding={['bottom', 'top']}
-//         id={props.id}
-//         background={props.background}>
-//         <GridBlock
-//           align="center"
-//           contents={props.children}
-//           layout={props.layout}
-//         />
-//       </Container>
-//     );
+    const Block = (props) => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
 
-//     const FeatureCallout = () => (
-//       <div
-//         className="productShowcaseSection paddingBottom"
-//         style={{textAlign: 'center'}}>
-//         <h2>Feature Callout</h2>
-//         <MarkdownBlock>These are features of this project</MarkdownBlock>
-//       </div>
-//     );
+    const FeatureCallout = () => (
+      <div
+        className="productShowcaseSection paddingBottom"
+        style={{textAlign: 'center'}}>
+        <h2>Feature Callout</h2>
+        <MarkdownBlock>These are features of this project</MarkdownBlock>
+      </div>
+    );
 
-//     const TryOut = () => (
-//       <Block id="try">
-//         {[
-//           {
-//             content:
-//               'To make your landing page more attractive, use illustrations! Check out ' +
-//               '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-//               'The illustrations you see on this page are from unDraw.',
-//             image: `${baseUrl}img/undraw_code_review.svg`,
-//             imageAlign: 'left',
-//             title: 'Wonderful SVG Illustrations',
-//           },
-//         ]}
-//       </Block>
-//     );
+    const TryOut = () => (
+      <Block id="try">
+        {[
+          {
+            content:
+              'The idea behind this little library is that by using it we can focus on building awesome things, instead of spending time on implementing simple and boring, but important and necessary validation functions in JS. Let\'s develop together so we can enjoy more time to build the really important things.',
+            image: `${baseUrl}img/undraw_to_the_moon.svg`,
+            imageAlign: 'left',
+            title: 'Focus on what matters',
+          },
+        ]}
+      </Block>
+    );
 
-//     const Description = () => (
-//       <Block background="dark">
-//         {[
-//           {
-//             content:
-//               'This is another description of how this project is useful',
-//             image: `${baseUrl}img/undraw_note_list.svg`,
-//             imageAlign: 'right',
-//             title: 'Description',
-//           },
-//         ]}
-//       </Block>
-//     );
+    const Description = () => (
+      <Block background="light">
+        {[
+          {
+            content:
+              'We invite you to collaborate in any way with this project, if you want to contribute with any suggestion or opinion, go ahead. We thank you in advance. This is just the beginning, let\'s grow together',
+            image: `${baseUrl}img/undraw_good_team.svg`,
+            imageAlign: 'right',
+            title: 'Collaborate',
+          },
+        ]}
+      </Block>
+    );
 
-//     const LearnHow = () => (
-//       <Block background="light">
-//         {[
-//           {
-//             content:
-//               'Each new Docusaurus project has **randomly-generated** theme colors.',
-//             image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
-//             imageAlign: 'right',
-//             title: 'Randomly Generated Theme Colors',
-//           },
-//         ]}
-//       </Block>
-//     );
+    const LearnHow = () => (
+      <Block background="light">
+        {[
+          {
+            content:
+              'We intend to offer a tiny solution that provides concrete and essential validations for projects that do not require out-of-the-ordinary validations. Currently, there are excellent libraries that offer large sets of functions to build validations, however, the focus here is to offer the essential ones to maintain a compact library.',
+            image: `${baseUrl}img/undraw_Code_typing.svg`,
+            imageAlign: 'right',
+            title: 'Why?',
+          },
+        ]}
+      </Block>
+    );
 
-//     const Features = () => (
-//       <Block layout="fourColumn">
-//         {[
-//           {
-//             content: 'This is the content of my feature',
-//             image: `${baseUrl}img/undraw_react.svg`,
-//             imageAlign: 'top',
-//             title: 'Feature One',
-//           },
-//           {
-//             content: 'The content of my second feature',
-//             image: `${baseUrl}img/undraw_operating_system.svg`,
-//             imageAlign: 'top',
-//             title: 'Feature Two',
-//           },
-//         ]}
-//       </Block>
-//     );
+    const Features = () => (
+      <Block layout="fourColumn">
+        {[
+          {
+            content: 'This is the content of my feature',
+            image: `${baseUrl}img/undraw_react.svg`,
+            imageAlign: 'top',
+            title: 'Focus on what matters',
+          },
+          {
+            content: 'The content of my second feature',
+            image: `${baseUrl}img/undraw_operating_system.svg`,
+            imageAlign: 'top',
+            title: 'Feature Two',
+          },
+        ]}
+      </Block>
+    );
 
-//     const Showcase = () => {
-//       if ((siteConfig.users || []).length === 0) {
-//         return null;
-//       }
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
 
-//       const showcase = siteConfig.users
-//         .filter((user) => user.pinned)
-//         .map((user) => (
-//           <a href={user.infoLink} key={user.infoLink}>
-//             <img src={user.image} alt={user.caption} title={user.caption} />
-//           </a>
-//         ));
+      const showcase = siteConfig.users
+        .filter((user) => user.pinned)
+        .map((user) => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
 
-//       const pageUrl = (page) =>
-//         baseUrl + (language ? `${language}/` : '') + page;
+      const pageUrl = (page) =>
+        baseUrl + (language ? `${language}/` : '') + page;
 
-//       return (
-//         <div className="productShowcaseSection paddingBottom">
-//           <h2>Who is Using This?</h2>
-//           <p>This project is used by all these people</p>
-//           <div className="logos">{showcase}</div>
-//           <div className="more-users">
-//             <a className="button" href={pageUrl('users.html')}>
-//               More {siteConfig.title} Users
-//             </a>
-//           </div>
-//         </div>
-//       );
-//     };
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using Delfos?</h2>
+          <p>Delfos is building validations for these projects...</p>
+          <div className="logos">{showcase}</div>
+        </div>
+      );
+    };
 
-//     return (
-//       <div>
-//         <HomeSplash siteConfig={siteConfig} language={language} />
-//         <div className="mainContainer">
-//           <Features />
-//           <FeatureCallout />
-//           <LearnHow />
-//           <TryOut />
-//           <Description />
-//           <Showcase />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <LearnHow />
+          <TryOut />
+          <Description />
+          <Showcase />
+        </div>
+      </div>
+    );
+  }
+}
 
-// module.exports = Index;
+module.exports = Index;
