717d537f396944d669ce4a1d5e690d382d92f144
Alvaro Araujo <alvfork@gmail.com>
2021-03-11 00:38:43 -0400

    refactor(object): use "is not null object assertion" in "is object empty" assertion

--- src/assertions/object/object.js
+++ src/assertions/object/object.js
@@ -7,24 +7,28 @@ import {isObject} from '../primitive/primitive';
  * @param {*} object
  * @returns {boolean}
  * @example
- * isEmptyObject({});  // true
+ * isNotNullObject({});                                    // true
+ * isNotNullObject({name: 'Delfos', version: 'v3.6.9'});   // true
+ * isNotNullObject(null);                                  // false
+ * isNotNullObject(undefined);                             // false
  */
 
 export const isNotNullObject = (object) => isObject(object) && object !== null;
 
 /**
- * Assert object type, non-null value and presence of at least one property.
+ * Assert object type, non-null value, and presence of at least one property.
  *
  * @function
  * @param {*} object
  * @returns {boolean}
  * @example
- * isEmptyObject({});    // true
- * isEmptyObject(null);  // false
- * isEmptyObject({name: 'Delfos', version: 'v3.6.9'});  // false
+ * isEmptyObject({});                                      // true
+ * isEmptyObject(null);                                    // false
+ * isEmptyObject(undefined);                               // false
+ * isEmptyObject({name: 'Delfos', version: 'v3.6.9'});     // false
  */
 
 export const isEmptyObject = (object) =>
-  isObject(object) && object !== null
+  isNotNullObject(object)
     ? object.constructor === Object && Object.keys(object).length === 0
     : false;
