24ac5136b49f9d3282f8e30a25e22ee39a0ef329
Luis Sanchez Eslaba <sanchez.luis122@gmail.com>
2021-03-12 22:40:15 -0400

    chore(docs): change bash script for generate md docs

--- gendoc.sh
+++ gendoc.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-function depth() {
-  if [[ -d $1 ]]; then
-    for insided in $1/* ; do (
-      if [[ -d $insided ]]; then
-        mkdir ./docs${insided##*/src}
-        depth "$insided"
-      fi
-      if [ ${insided#*.test} != ".js" ]; then
-        path=${insided##*/src}
-        finalpath="$(echo "$path" | sed 's/\/[a-zA-Z]*.js//')"
-        documentation build $insided -f md --shallow > ./docs$finalpath/doc.md
-        title="${finalpath##*/}"
-        echo -e "---\ntitle: $title\nsidebar_label: $title\n---\n$(cat ./docs$finalpath/doc.md)" > ./docs$finalpath/doc.md
-      fi
-    ); done
-  fi
-}
-
-find docs/* -maxdepth 0 -type d -exec rm -rf "{}" \;
-echo "Generating docs..."
+find ./docs/* ! -name 'doc.md' -exec rm -rf "{}" \;
+echo "Delfos | Generating docs..."
 for srcd in ./src/* ; do (
-  if [[ -d $srcd ]]; then
-    mkdir ./docs/${srcd##*/}
-    for insided in $srcd/* ; do (
-      if [[ -d $insided ]]; then
-        mkdir ./docs${insided##*/src}
-        depth "$insided"
-      fi
-    ); done
+  file="${srcd##*/}"
+  title="${file%.*}"
+  if [ $title != "index" ]; then
+    documentation build $srcd -f md --shallow > ./docs/$title.md
+    echo -e "---\ntitle: $title\nsidebar_label: $title\n---\n$(cat ./docs/$title.md)" > ./docs/$title.md
   fi
 ); done
+echo -e '\e[1A\e[KDelfos | Docs generated.'
